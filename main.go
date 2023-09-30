fa8a30dfb07cbf33fdad4017d035bf44f088f5b6
Marius Fagerland <marius.fagerland@sparebank1.no>
2023-09-30 16:27:06 +0200

    Refactor

--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"database/sql"
-
 	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
 )
-
-type Todo struct {
-	Task string `json:"task"`
-}
 
 func setupRouter(todoRepository TodoRepositoryContract) *gin.Engine {
 	router := gin.Default()
@@ -21,40 +14,6 @@ func setupRouter(todoRepository TodoRepositoryContract) *gin.Engine {
 		todoPostHandler(c, todoRepository)
 	})
 	return router
-}
-
-type TodoRepositoryContract interface {
-	Insert(string) error
-	GetAll() ([]Todo, error)
-}
-
-type TodoRepository struct {
-	db *sql.DB
-}
-
-func (todoRepository *TodoRepository) Insert(task string) error {
-	_, err := todoRepository.db.Exec("INSERT INTO todos (task) VALUES ($1)", task)
-	return err
-}
-
-func (todoRepository *TodoRepository) GetAll() ([]Todo, error) {
-	rows, err := todoRepository.db.Query("SELECT task FROM todos")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var todos []Todo
-
-	for rows.Next() {
-		var todo Todo
-		err := rows.Scan(&todo.Task)
-		if err != nil {
-			return nil, err
-		}
-		todos = append(todos, todo)
-	}
-	return todos, nil
 }
 
 func main() {
