28c1fc6f66fc2fdbde79a557722eac84e5b77101
Marius Fagerland <marius.fagerland@sparebank1.no>
2023-09-30 16:35:53 +0200

    Update postgres-start.sh

--- postgres-start.sh
+++ postgres-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 
-docker run --rm -p 5432:5432 --name postgres-smak -e POSTGRES_PASSWORD=password  postgres
+docker run --rm -p 5432:5432 --name postgres-todo -e POSTGRES_PASSWORD=password  postgres
