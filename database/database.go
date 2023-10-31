7d121aa615fa011fb99248624dcf218a86844618
Marius Fagerland <marius.fagerland@sparebank1.no>
2023-10-31 15:03:40 +0100

    Finish migrate

--- database/database.go
+++ database/database.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"todo/config"
+
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
@@ -11,4 +13,13 @@ import (
 func Migrate(dbConfig config.DbConfig, path string) error {
 	connectionString := dbConfig.GetConnectionString()
 
+	basePath := fmt.Sprintf("file://%s/base", path)
+	m, err := migrate.New(basePath, connectionString)
+	if err != nil {
+		return err
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
 }
