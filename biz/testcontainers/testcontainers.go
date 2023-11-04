90830574dd52a9b24e99e5bbc3bb18280b0c669a
Marius Fagerland <marius.fagerland@sparebank1.no>
2023-11-04 13:19:35 +0100

    Create testcontainers

---
+++ biz/testcontainers/testcontainers.go
@@ -0,0 +1,30 @@
+package testcontainers
+
+import (
+	"context"
+	"time"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func CreateTestContainer() (testcontainers.Container, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:15-alpine",
+		ExposedPorts: []string{"5432/tcp"},
+		Env: map[string]string{
+			"POSTGRES_USER":     "test",
+			"POSTGRES_PASSWORD": "test",
+			"POSTGRES_DB":       "test",
+		},
+		WaitingFor: wait.ForListeningPort("5432/tcp").WithStartupTimeout(time.Duration(5 * time.Second)),
+	}
+	container, err := testcontainers.GenericContainer(context.Background(), testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return container, nil
+}
