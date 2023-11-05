7b7421edf1a8819440b329bfb047ac3984f35a08
Marius Fagerland <marius.fagerland@sparebank1.no>
2023-11-05 14:56:41 +0100

    Add app setup

---
+++ app/app.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"database/sql"
+	"todo/biz/adapters"
+	"todo/biz/repositories"
+	"todo/biz/services"
+	"todo/rest/controllers"
+	"todo/rest/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AppSetup(db *sql.DB) *gin.Engine {
+	repositories := repositories.NewRepositories(db)
+	adapters := adapters.NewAdapterFakes()
+	services := services.NewServices(repositories, adapters)
+	controllers := controllers.NewControllers(services)
+	return routes.SetupRoutes(controllers)
+}
+
+func AppTestSetup() (*gin.Engine, *repositories.Repositories) {
+	repositories := repositories.NewRepositoriesFake()
+	adapters := adapters.NewAdapterFakes()
+	services := services.NewServices(repositories, adapters)
+	controllers := controllers.NewControllers(services)
+	return routes.SetupRoutes(controllers), repositories
+}
