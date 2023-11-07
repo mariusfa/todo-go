a0cd153f6512d98e85e36ffc1b9b42ef7fb58a0c
Marius Fagerland <mariusfagerland@yahoo.no>
2023-11-07 20:01:33 +0100

    Impl test

--- biz/user/user_adapter_test.go
+++ biz/user/user_adapter_test.go
@@ -15,9 +15,22 @@ func TestUserAdapterGet(t *testing.T) {
 		httpmock.NewJsonResponderOrPanic(http.StatusOK, httpmock.File("./users.json")))
 
 
-	// fetch json using User adapter
+	userAdapter := NewUserAdapter()
+	users, err := userAdapter.GetAll()
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
 
-	// assert
+	if len(users) != 2 {
+		t.Errorf("Expected 2 users, got %v", len(users))
+	}
 
+	if users[0].ID != 1 {
+		t.Errorf("Expected ID 1, got %v", users[0].ID)
+	}
+
+	if users[0].Name != "John Doe" {
+		t.Errorf("Expected Name John Doe, got %v", users[0].Name)
+	}
 }
 
